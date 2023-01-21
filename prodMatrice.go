9b66fdf640293c3a67536316c621bc0bae252765
nicoflux <nicolas.bloch1234@gmail.com>
2023-01-21 17:50:03 +0100

    écriture résultat dans un fichier

--- prodMatrice.go
+++ prodMatrice.go
@@ -1,8 +1,8 @@
-//produit matriciel en go quu marche
 package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
 	"strconv"
@@ -10,8 +10,8 @@ import (
 )
 
 type sendJob struct {
-	pointA *[][]int
-	pointB *[][]int
+	pointA *Matrice
+	pointB *Matrice
 	i, j   int
 }
 
@@ -19,29 +19,35 @@ type endJob struct {
 	x, y, z int
 }
 
+type Matrice [][]int
+
 var matFile1 string = "matrice1.txt"
 var matFile2 string = "matrice2.txt"
+var resultProduit string = "resultProd.txt"
 
 func calcul(jobChan chan sendJob, resultChan chan endJob) {
+
 	for true {
 		job := <-jobChan
 		var res int
 		var m1 = *(job.pointA)
 		var m2 = *(job.pointB)
+
 		for k := 0; k < len(m2); k++ {
 			res += m1[job.i][k] * m2[k][job.j]
 		}
+
 		result := endJob{job.i, job.j, res}
 		resultChan <- result
 	}
 }
 
-func prodMat(MatA, MatB [][]int) [][]int {
+func prodMat(MatA, MatB Matrice) {
 
 	var ligne int = len(MatA)
 	var col int = len(MatB[0])
 
-	resultat := make([][]int, ligne)
+	resultat := make(Matrice, ligne)
 	for m := range resultat {
 		resultat[m] = make([]int, col)
 	}
@@ -53,7 +59,7 @@ func prodMat(MatA, MatB [][]int) [][]int {
 		go calcul(jobChan, resultChan)
 	}
 
-	go func(MatA, MatB [][]int) {
+	go func(MatA, MatB Matrice) { //on push des jobs de structure sendJob dans le channel jobChan
 		for i := 0; i < ligne; i++ {
 			for j := 0; j < col; j++ {
 				job := sendJob{&MatA, &MatB, i, j}
@@ -62,80 +68,102 @@ func prodMat(MatA, MatB [][]int) [][]int {
 		}
 	}(MatA, MatB)
 
-	for b := 0; b < 16; b++ {
+	for b := 0; b < 16; b++ { //on vide le channel de resultat sous forme de structure endJob
 		resultJob := <-resultChan
 		resultat[resultJob.x][resultJob.y] = resultJob.z
 
 	}
 	//close(resultChan)
 	//close(jobChan)
-	return resultat
+	writeMatrice(resultat)
 }
 
-func readMatrice(fileMat string) [][]int {
+func readMatrice(fileMat string) Matrice {
 	file, err := os.Open(fileMat)
 	if err != nil {
 		log.Fatal(err) // affiche l'erreur et quitte la fonction
 	}
-	defer file.Close()
 
-	scanner := bufio.NewScanner(file) // Créez un nouveau scanner pour lire le fichier
-	var matrice [][]int               // Déclarez une variable pour stocker la matrice
+	scanner := bufio.NewScanner(file) // Crée un nouveau scanner pour lire le fichier
+	var matrice Matrice
 
 	for scanner.Scan() { // Pour chaque ligne du fichier...
 
-		line := scanner.Text()               // Obtenez la ligne courante
-		elements := strings.Split(line, " ") // Découpez la ligne en une liste d'éléments
+		line := scanner.Text()               // Obtient la ligne courante
+		elements := strings.Split(line, " ") // Découpe la ligne en une liste d'éléments
 
-		var row []int // Pour chaque élément de la ligne...
-		for _, element := range elements {
+		var row []int
+		for _, element := range elements { // Pour chaque élément de la ligne
 
-			num, err := strconv.Atoi(element) // Convertir l'élément en entier
+			num, err := strconv.Atoi(element) // Convertit l'élément en entier
 			if err != nil {
 				log.Fatal(err)
 			}
 
-			// Ajoutez l'élément converti à la ligne
+			// Ajoute l'élément converti à la ligne
 			row = append(row, num)
 		}
 
-		// Ajoutez la ligne à la matrice
+		// Ajoutezla ligne à la matrice
 		matrice = append(matrice, row)
 	}
 	return (matrice)
 }
 
-func verifMat(MatA, MatB [][]int) bool {
+func verifMat(MatA, MatB Matrice) bool {
 	if len(MatA[0]) != len(MatB) {
 		return false
 	}
 	return true
 }
 
+func writeMatrice(matrice Matrice) {
+	file, err := os.Create(resultProduit)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	ligne := len(matrice)
+	col := len(matrice[0])
+	for i := 0; i < ligne; i++ {
+		for j := 0; j < col-1; j++ {
+			fmt.Fprint(file, matrice[i][j], " ")
+		}
+		fmt.Fprint(file, matrice[i][col-1]) //si on laisse le dernier element de la ligne dans la boucle, un espacé est écrit après et on arrive pas a lire le fichier
+		fmt.Fprintln(file)
+	}
+}
+
+func afficheMatrice(matrice Matrice) {
+	ligne := len(matrice)
+	col := len(matrice[0])
+
+	for i := 0; i < ligne; i++ {
+		for j := 0; j < col; j++ {
+			print((matrice[i][j]))
+			if matrice[i][j] < 10 {
+				print("  ")
+			} else {
+				print(" ")
+			}
+		}
+		println()
+	}
+}
+
 func main() {
-	//var mat1 = [][]int{{1, 1, 1, 1}, {2, 2, 2, 2}, {3, 3, 3, 3}, {4, 4, 4, 4}}
-	//var mat2 = [][]int{{4, 4, 4, 4}, {3, 3, 3, 3}, {2, 2, 2, 2}, {1, 1, 1, 1}}
+	//var mat1 = Matrice{{1, 1, 1, 1}, {2, 2, 2, 2}, {3, 3, 3, 3}, {4, 4, 4, 4}}
+	//var mat2 = Matrice{{4, 4, 4, 4}, {3, 3, 3, 3}, {2, 2, 2, 2}, {1, 1, 1, 1}}
 
-	var mat1 [][]int = readMatrice(matFile1)
-	var mat2 [][]int = readMatrice(matFile2)
-	var ligne int = len(mat1)
-	var col int = len(mat2)
+	var MatA Matrice = readMatrice(matFile1)
+	var MatB Matrice = readMatrice(matFile2)
 
-	if !verifMat(mat1, mat2) {
+	if !verifMat(MatA, MatB) {
 		print("Erreur : impossible de multriplier les matrices.\nVeuillez verifier leurs dimensions\n")
 	} else {
-		var result = prodMat(mat1, mat2)
-		print("Le produit de la matrice A par la matrice B est :\n")
-		for i := 0; i < ligne; i++ {
-			for j := 0; j < col; j++ {
-				print((result[i][j]))
-				if result[i][j] < 10 {
-					print("  ")
-				} else {
-					print(" ")
-				}
-			}
-			println()
-		}
+		prodMat(MatA, MatB)
+		resultat := readMatrice(resultProduit)
+		afficheMatrice((resultat))
 	}
 }
